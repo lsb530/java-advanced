6a1322e49b7b6a9cd6b6a0fa5589aaec009717bd
boki <lsb530@naver.com>
2024-11-28 15:18:40 +0900

    CAS 락 구현2

--- java-adv-1/src/thread/cas/spinlock/SpinLockMain.java
+++ java-adv-1/src/thread/cas/spinlock/SpinLockMain.java
@@ -1,17 +1,20 @@
 package thread.cas.spinlock;
 
 import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
 
 public class SpinLockMain {
 
     public static void main(String[] args) {
-        SpinLockBad spinLock = new SpinLockBad();
+        // SpinLockBad spinLock = new SpinLockBad();
+        SpinLock spinLock = new SpinLock();
 
         Runnable task = () -> {
             spinLock.lock();
             try {
                 // critical section
                 log("비즈니스 로직 실행");
+                sleep(10); // 오래 걸리는 로직에서 스핀 락 사용 X
             } finally {
                 spinLock.unlock();
             }
